c32f2c29fafc3be63e044cfbdf8d5cd39a9d7890
unknown <jessica.gray@bryanuniversity.edu>
2020-04-19 18:25:22 -0400

    basicwebsit

---
+++ basic website.js
@@ -0,0 +1,16 @@
+var hello = document.createElement("hello")
+document.body.append(hello)
+hello.textContent = "fsw-110 is truly a delight"
+var nav = document.createElement("navbar")
+
+document.body.append(nav)
+nav.textContent ="contact us, you know you want to"
+    var newp = document.createElement("p")
+    document.body.append(newp)
+    newp.textContent = "I love learning new things!"
+var Alist = document.createElement("list")
+document.body.append(Alist)
+Alist.textContent = "sea salt carmel chocolate"
+var footer = document.createElement("footer")
+document.body.append(footer)
+footer.textContent = "ta ta for now"
