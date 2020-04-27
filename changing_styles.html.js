93833a70c22dc8e92ca44c6426a5a1d51b5fb36d
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-04-27 17:36:41 -0400

    Week3 JS

---
+++ changing_styles.html.js
@@ -0,0 +1,9 @@
+var header = document.getElementById("header")
+
+header.style.fontSize ="20px"
+header.style.fontFamily ="sans-serif"
+header.style.fontWeight ="lighter"
+header.style.color ="cornflowerblue"
+
+
+
