878d16f0014fb60e13e755ad360bd2fe181f1b09
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-05-10 19:53:21 -0400

    Add files via upload

---
+++ Travel_Form.js
@@ -0,0 +1,9 @@
+const form = document.myForm
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+ {
+    var x = document.getElementById("myImg").src;
+    document.getElementById("myImg").innerHTML = x;
+      }
